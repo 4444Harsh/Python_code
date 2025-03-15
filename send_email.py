063897c992556e324701ed5f9a88f3b30f02615b
Harsh <128832204+4444Harsh@users.noreply.github.com>
2025-03-15 23:51:10 +0530

    Create send_email.py

---
+++ send_email.py
@@ -0,0 +1,16 @@
+import smtplib
+
+to= input("Enter the email of recipent: \n")
+
+content= input("Enter the content for mail;\n")
+
+def sendEmail(to ,content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('senderemail@gmail.com','1234')
+    server.sendmail('senderemail@gmail.com', to, content)
+    
+    server.close()
+    
+sendEmail(to,content)
